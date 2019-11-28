e0d318f71529df01df2488b616cc2f19ed7421b7
mufaddal29 <52793669+mufaddal29@users.noreply.github.com>
2019-11-28 06:56:16 +0530

    Add files via upload

---
+++ Capstone project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# ## This notebook will be mainly used for the capstone project
+
+# In[4]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
